f57c211a96464103d38374fcb1a0a58b129e01ad
germanagca85-commits <german.agca85@gmail.com>
2026-09-13 21:55:29 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE = 'pase-lista-qr-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Red primero (para que siempre veas la versión más reciente), y si no hay
+// internet, usa lo que quedó guardado en caché (funciona sin conexión).
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const resClone = res.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, resClone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
